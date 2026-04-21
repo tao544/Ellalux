bbb62a19ed899e74b78764336031dc314271fb27
tao544 <taoheedadepoju1@gmail.com>
2026-04-21 22:30:41 +0100

    still preparing

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 pip install --upgrade pip
-pip install --upgrade setuptools
+pip install setuptools wheel
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
