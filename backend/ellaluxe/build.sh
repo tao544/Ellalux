bc581cccff4512922fa3548165d002071a0f761d
tao544 <taoheedadepoju1@gmail.com>
2026-04-21 01:43:58 +0100

    fix pkg resource for render

--- backend/ellaluxe/build.sh
+++ backend/ellaluxe/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+pip install --upgrade pip
+pip install --upgrade setuptools
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
